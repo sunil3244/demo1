1abbdc0d648619295fb4c070d6bf2a69ddac76aa
sunil3244 <91534862+sunil3244@users.noreply.github.com>
2021-09-28 16:47:50 +0530

    java program

---
+++ Que_01_sum_of_10_natural_no.java
@@ -0,0 +1,18 @@
+
+//Q1 Write a program to calculate the sum of first 10 natural number.
+package assignment_02;
+
+public class Que_01_sum_of_10_natural_no {
+
+	public static void main(String[] args) 
+	{
+		int num=0;
+		for(int i=1;i<=10;i++)
+		{
+		num=num+i;
+		
+		}
+		System.out.println("Sum of 10 natural no="+num);
+	}
+
+}
